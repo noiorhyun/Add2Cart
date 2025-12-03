b11f24dd74b6ecbdbf823973204402768d5d6206
Jane <noiorhyun@gmail.com>
2025-12-02 16:14:46 -0800

    add order class

--- models.py
+++ models.py
@@ -7,3 +7,13 @@ class Product:
     
     def __repr__(self):
         return f"{self.id} | {self.name} | ${self.price:.2f} | ⭐ {self.rating}"
+
+class Order:
+    def __init__(self, order_id, items):
+        self.order_id = order_id
+        self.items = items
+        self.total_price = sum(p.price for p in items)
+
+    def __repr__(self):
+        item_names = ", ".join(p.name for p in self.items)
+        return f"Order #{self.order_id}: {item_names} | Total: ${self.total_price:.2f}"
