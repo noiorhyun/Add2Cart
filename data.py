4708f5be6944cfdd760912c52bf15c58ad2e39a2
Jane <noiorhyun@gmail.com>
2025-12-02 16:16:05 -0800

    Create product catalog

---
+++ data.py
@@ -0,0 +1,9 @@
+from models import Product
+
+products = [
+    Product(101, "Keyboard", 39.99, 4.3),
+    Product(102, "Mouse", 19.99, 4.1),
+    Product(103, "Monitor", 129.99, 4.7),
+    Product(104, "USB Cable", 9.99, 4.0),
+    Product(105, "Laptop Stand", 29.99, 4.6),
+]
